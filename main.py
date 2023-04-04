0a38e4b9f28611a6a79d6b3bcc853d84cc92acc4
Daniel Topa <122997699+DenyT17@users.noreply.github.com>
2023-04-04 18:47:56 +0200

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from function import predict_video
-
-
-predict_video(r"C:\Users\Dtopa\OneDrive\Pulpit\Image Classification\Sport Classfication\Videos\Most SPECTACULAR SKILLS of the Season.mp4",
-        r"C:\Users\Dtopa\OneDrive\Pulpit\Objec Detector\Football_Object_Detection\runs\detect\train2\weights\best.pt")
